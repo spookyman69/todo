6acff944b9f45e3a4241b8928001548be6734f5b
harisankar <harisankarsaketham513@gmail.com>
2023-07-25 16:43:22 +0530

    todo project first commit

---
+++ todoapp/forms.py
@@ -0,0 +1,9 @@
+from todoapp.models import Task
+from django import forms
+
+
+class TodoForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['name', 'priority', 'date']
+
